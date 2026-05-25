c5c682b516cc25a3ce66accf5a71b61b2ba735d5
Marcus Guedes <mclguedes@gmail.com>
2026-05-25 10:57:59 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -39,7 +39,7 @@ st.markdown("---")
 @st.cache_data
 def carregar_dados():
     df = pd.read_excel(
-        'PLANILHA DE RECOLHA DE NOTAS MAIO.xlsx',
+        'PLANILHA DE RECOLHA DE NOTAS MAIO',
         engine='openpyxl'
     )
     # Renomeia as colunas
